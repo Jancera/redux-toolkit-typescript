f6dc8c3a77b8cea932ede40985309545a0fe501b
Jancer Lima <contato.jancersl@gmail.com>
2022-07-01 23:31:19 -0300

    Changing types

--- src/store/user/slice.ts
+++ src/store/user/slice.ts
@@ -1,16 +1,20 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {
+  createSlice,
+  PayloadAction,
+  SliceCaseReducers,
+} from "@reduxjs/toolkit";
 import { userInitialState } from "./initialState";
 import { IUser } from "./interfaces/IUser";
 
-export const userSlice = createSlice({
+export const userSlice = createSlice<IUser, SliceCaseReducers<IUser>>({
   name: "user",
   initialState: userInitialState,
   reducers: {
-    userSetName(state: IUser, action: PayloadAction<string>) {
+    userSetName(state, action: PayloadAction<string>) {
       console.log(action);
       state.name = action.payload;
     },
-    userSetAge(state: IUser, action: PayloadAction<string>) {
+    userSetAge(state, action: PayloadAction<string>) {
       state.age = action.payload;
     },
   },
